68849a3a02d1ca07977081cca9e3687ccb8d2767
ML <0XXXXX.ML.XXXXX0@gmail.com>
2024-08-16 22:09:43 -0600

    Update Plague.js

--- Plague.js
+++ Plague.js
@@ -1,7 +1,7 @@
 var map = {
-    title: "Plague",
+    title: "Test Map V8",
     song: "env2",
-    maker: "Pluh",
+    maker: "Ice dodo map maker",
     spawn: [0, 0.5, 0],
     init: function() {
         a.y([-1.52993, -2.28993, -18.05993], [2.09, 0, 0], [4.02, 0.12, 4.02], "05070a", 0, 0, 0.6, false, 1, true);
